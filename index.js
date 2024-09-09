f9edd2a80a590156fa04d84e2897f868ebfc8f90
harjeevansingh <harjeevansingh01@gmail.com>
2024-09-09 14:26:16 +0530

    feat: added basic player setup

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
+import TrackPlayer from 'react-native-track-player';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
+import { playbackService } from './musicPlayerServices';
 
 AppRegistry.registerComponent(appName, () => App);
+
+// AppRegistry.registerComponent(...);
+TrackPlayer.registerPlaybackService(() => playbackService);
